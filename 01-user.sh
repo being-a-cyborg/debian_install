c4686157b02888fc054a5d8ef571a85ecef209ea
Alex M. Rusetsky <rusetsky@yandex.ru>
2025-09-28 10:03:59 +0300

    01

--- 01-user.sh
+++ 01-user.sh
@@ -3,6 +3,6 @@
 user=support
 temp_pass=12345
 
-useradd -m -d /home/"$user" $user
+useradd -m -d /home/"$user" -s /bin/bash $user
 echo $temp_pass | passwd $user --stdin
 chage -d 0 $user
---
+++ 03-soft.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# mics
+
+sudo apt install \
+  mc \
+  neovim \
+  tcpdump \
+  dunst
+
+-yy
