d492dd39ee1ca98d2767a95d40f0092c3618c692
Alex M. Rusetsky <rusetsky@yandex.ru>
2025-10-03 17:24:52 +0300

    04,05,06,07

--- 03-soft.sh
+++ 03-soft.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
 # mics
+sudo apt install curl gnome-keyring pass bash-doc mpc tree ncmpcpp mpd tmux lazygit lsd stow mc neovim tcpdump dunst build-essential -yy
 
-sudo apt install mpc tree ncmpcpp mpd tmux lazygit lsd stow mc neovim tcpdump dunst build-essential -yy
+# microcode
+# sudo apt install intel-microcode -yy
+
+# X + suckless
+sudo apt install libharfbuzz-dev libxrandr-dev libcairo2-dev libxft-dev libx11-dev libxinerama-dev -yy
+
+# ly
+sudo apt install libpam0g-dev libxcb-xkb-dev xauth xserver-xorg brightnessctl
