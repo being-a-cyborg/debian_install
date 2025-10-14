209ba6d33d5eee612f1f2d772b3240d699f698da
Alex M. Rusetsky <rusetsky@yandex.ru>
2025-10-14 08:28:08 +0300

    008

--- 008-fonts.sh
+++ 008-fonts.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
-sudo apt install ttf-mscorefonts-installer fonts-roboto -yy
+#sudo apt install ttf-mscorefonts-installer fonts-roboto -yy
+ 
+#wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Terminus.tar.xz
+#mkdir -v -p /usr/share/fonts/truetype/TerminusNF
+#sudo tar xvf Terminus.tar.xz -C /usr/share/fonts/truetype/TerminusNF/
+#rm -f Terminus.tar.xz
 
-#wget https://github.com/ryanoasis/nerd-fonts/releases/downLoad/v3.4.0/Terminus.tar.xZ
 #wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/RobotoMono.tar.xz
-#wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/VictorMono.tar.xz
-# blob:https://github.com/0852b493-71cd-4f66-8d83-1b3d401a0f5a
-#wget https://github.com/ryanoasis/nerd-fonts/blob/master/install.sh
-# ./install.sh Terminus
-# ./install.sh Roboto
-# ./install.sh VictorMono
+#sudo mkdir -v -p /usr/share/fonts/truetype/RobotoMonoNF
+#sudo tar xvf RobotoMono.tar.xz -C /usr/share/fonts/truetype/RobotoMonoNF/
+#rm -f RobotoMono.tar.xz
 
-sudo mkdir -v -p /usr/share/fonts/truetype/{Terminus,RobotoMono,VictorMono}
+#wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/VictorMono.tar.xz
+#sudo mkdir -v -p /usr/share/fonts/truetype/VictorMonoNF
+#sudo tar xvf VictorMono.tar.xz -C /usr/share/fonts/truetype/VictorMonoNF/
+#rm -f VictorMono.tar.xz
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/DejaVuSansMono.zip
+sudo mkdir -v -p /usr/share/fonts/truetype/DejaVuSansMonoNF
+sudo unzip DejaVuSansMono.zip -d /usr/share/fonts/truetype/DejaVuSansMonoNF
+rm -f DejaVuSansMono.zip
